c31619d7150f22725dc13f2e5c70b1e8cf3fbca5
Kobwe Ko <53431391+kobwe@users.noreply.github.com>
2021-11-18 21:07:45 +0000

    nothing

--- main.py
+++ main.py
@@ -20,30 +20,42 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
-print("Welcome to Treasure Island.")
+print("Welcome to Treasure Space.")
 print("Your mission is to find the treasure.") 
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-
-first_pick = input ("You are stolen by the aliens! choose, what you need to do, hijack their UFO or swear them? (print hijack or swear) ").lower()
+first_pick = input ("You are stolen by the aliens! choose, what you need to do, hijack their UFO or swear them? (print hijack or swear) \n").lower()
 if first_pick == "swear":
-  print ("They drop you into ocean, game over")
-else:
+  print ("They drop you into the ocean, game over")
+elif first_pick == "hijack":
   second_pick = input ("You stole the UFO! Now you must choose where you must fly, to Mars or Venus? \n").lower()
+
   if second_pick == "venus":
     print ("Oh no, you trapped to the evil aliens :(\n")
-  else:
+  elif second_pick == "mars":
     third_pick = input("Wow! There are so much good aliens! They say that you are their hero and you must kill the dangerous dragon! Which weapon do you choose, sword, railgun or cheese?\n").lower()
+
     if third_pick == "sword":
       print ("oh no, the skin of this dragon is really strong, he eat you!")
     elif third_pick == "railgun":
       print ("its a useless weapon! All bullets ricochet in you!")
-    else:
-      print ("Wow! This dragon really love cheese you bring! He give you all treausers he got!\n")
+    elif third_pick == "cheese":
+      print ("Wow! This dragon really love the cheese which you bring! He give you all treausers he got!\n")
       print ("You win!")
+
+    else:
+      print ("You pick holy item of the aliens! They drop you into jail!")
+  else:
+    print ("Oh no, you pick different planet! Evil aliens catch you in space!")  
+else:
+  print ("You must play this game!")
+
       
+#im really sorry if your native language is English, its probably really hard to read this)
+
+ 
 
 
 
